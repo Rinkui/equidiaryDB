c99d59f08ad884f34b3e51c72d99e793efae09a3
Xenanex <anexlecool@gmail.com>
2019-10-12 18:26:26 +0200

    Add a second path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7001);
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users/:name", ctx -> ctx.result("Hello " + ctx.pathParam("name")));
     }
 }
